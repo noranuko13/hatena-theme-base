c93d75cb23970cfd9206163dd1382f07d704440f
noranuko13 <noranuko13@gmail.com>
2021-04-22 09:59:48 +0900

    :notes: [scripts] ログメッセージのデフォルト値を設定する

--- scripts/core/logger.js
+++ scripts/core/logger.js
@@ -1,13 +1,29 @@
+/**
+ * ログ
+ */
 class Logger {
-  static warn(title, messages) {
+  /**
+   * @param {string} title タイトル
+   * @param {string[]} messages メッセージ
+   */
+  static warn(title, messages = []) {
     Logger.log('33', `[注意] ${title}`, messages);
   }
 
-  static info(title, messages) {
+  /**
+   * @param {string} title タイトル
+   * @param {string[]} messages メッセージ
+   */
+  static info(title, messages = []) {
     Logger.log('34', `[情報] ${title}`, messages);
   }
 
-  static log(color, title, messages) {
+  /**
+   * @param {string} color ログの色(ANSI escape code)
+   * @param {string} title ログのタイトル
+   * @param {string[]} messages ログのメッセージ
+   */
+  static log(color, title, messages = []) {
     /* eslint-disable no-console */
     console.log(`\x1b[${color}m${title}\x1b[0m`);
     console.group();
